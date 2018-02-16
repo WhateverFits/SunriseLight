cceda54cce7bcc51c253f00b5966eb9483bfd51a
Sean Walker <me@seanwalker.org>
2018-02-16 12:14:31 -0800

    Lots of work
    Set pins, added alarms, made use daylight saving, grab sunrise/sunset times from service, added moonrise.

--- Sunrise.h
+++ Sunrise.h
@@ -1,3 +1,5 @@
+#include <Adafruit_NeoPixel.h>
+
 class Sunrise {
   private:
     int _delay;
@@ -11,37 +13,63 @@ class Sunrise {
     bool showSunset = false;
     bool showMoon = false;
 
-    Adafruit_NeoPixel strip;
+    Adafruit_NeoPixel *strip = NULL;
     
     void sunrise() {
       if (R < 255)
         R++;
-      if (R > 50 && G < 255)
+      if (R > 20 && G < 255)
         G++;
       if (R > 100 && B < 255)
         B++;
       for (int i = 0; i < numLeds; i++) {
-        strip.setPixelColor(i, R, G, B);   
+        strip->setPixelColor(i, R, G, B);   
       }
     }
   
   void sunset() {
       if (R > 0 && G < 155)
         R--;
-      if (G > 0 && B < 205)
+      if (G > 0 && B < 235)
         G--;
       if (B > 0)
         B--;
       for (int i = 0; i < numLeds; i++) {
-        strip.setPixelColor(i, R, G, B);  
+        strip->setPixelColor(i, R, G, B);  
       }   
   }
 
+  void moonrise() {
+      if (B < 255)
+        B++;
+      if (B > 20 && G < 80)
+        G++;
+      if (B > 20 && R < 80)
+        R++;
+      for (int i = 0; i < numLeds; i++) {
+        strip->setPixelColor(i, R, G, B);   
+      }
+    }
+
+  void moonset() {
+      if (B > 0 && G < 80)
+        B--;
+      if (G > 0)
+        G--;
+      if (R > 0)
+        R--;
+      for (int i = 0; i < numLeds; i++) {
+        strip->setPixelColor(i, R, G, B);   
+      }
+    }
+
   public: 
-    Sunrise(int Delay, Adafruit_NeoPixel Strip, int NumLeds) {
+    Sunrise(int Delay, int NumLeds, int pin) {
       _delay = Delay;
-      strip = Strip;
       numLeds = NumLeds;
+      strip = new Adafruit_NeoPixel(NumLeds, pin, NEO_GRB + NEO_KHZ800);
+      strip->begin();
+      strip->show();
     }
 
     void StartSunrise() {
@@ -58,6 +86,20 @@ class Sunrise {
       startTime = millis();
     }
     
+    void StartMoonrise() {
+      showSunrise = false;
+      showSunset = false;
+      showMoon = true;
+      startTime = millis();
+    }
+    
+    void StartMoonset() {
+      showSunrise = false;
+      showSunset = false;
+      showMoon = false;
+      startTime = millis();
+    }
+    
     void Update() {
         unsigned long current = millis();
         if (current - startTime > _delay) {
@@ -65,7 +107,11 @@ class Sunrise {
             sunrise();
           else if (showSunset)
             sunset();
-          strip.show();
+          else if (showMoon)
+            moonrise();
+          else
+            moonset();
+          strip->show();
           startTime = current;
         }
     }
--- SunriseLight.ino
+++ SunriseLight.ino
@@ -1,105 +1,152 @@
+#include <ArduinoJson.h>
+
 #include <TimeAlarms.h>
 
 #include <Time.h>
 #include <TimeLib.h>
+#include <TimeAlarms.h>
 #include <Timezone.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-#include <Adafruit_NeoPixel.h>
 #include "Sunrise.h"
 
 
-#define DEBOUNCE 150
+#define DEBOUNCE 50
+#define REPEATDELAY 1000
 #define DELAY 1000
-#define LEDS 200
+#define LEDS 60
 #define localport 8888
-const char ssid[] = "*************";  //  your network SSID (name)
-const char pass[] = "********";       // your network password
-IPAddress timeServer(192, 168, 80, 1);
-//const int timeZone = -8;  // Pacific Standard Time (USA)
-//const int timeZone = -7;  // Pacific Daylight Time (USA)
+#define NEO_PIN D6
+#define BUTTON_PIN D2
 
+//const char* ssid = "The Ranch-2.4";  //  your network SSID (name)
+//const char* pass = "916-955-0942";       // your network password
+//const char* ssid = "ThunderLizard";  //  your network SSID (name)
+//const char* pass = "SnarfSnarf";       // your network password
+const char* ssid = "Acrid";  //  your network SSID (name)
+const char* pass = "MyVoiceIsMyPassport";       // your network password
+//const char* ssid = "MakerHQ";  //  your network SSID (name)
+//const char* pass = "sacramentomaker916";       // your network password
+IPAddress timeServer(192, 168, 80, 1);
+//IPAddress timeServer(63, 224, 11, 139);
+const char* ntpServerName = "pool.ntp.org";
 TimeChangeRule myDST = {"PDT", Second, Sun, Mar, 2, -420};    //Daylight time = UTC - 7 hours
 TimeChangeRule mySTD = {"PST", First, Sun, Nov, 2, -480};     //Standard time = UTC - 8 hours
 Timezone myTZ(myDST, mySTD);
 
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, 7, NEO_GRB + NEO_KHZ800);
-Sunrise sunrise(DELAY, strip, LEDS);
+Sunrise sunrise = Sunrise(DELAY, LEDS, NEO_PIN);
 WiFiUDP Udp;
 unsigned int localPort = 8888;  // local port to listen for UDP packets
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Setup start");
 
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+unsigned long lastButtonTime[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+unsigned long lastButtonState[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+unsigned long buttonState[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-  WiFi.begin(ssid, pass);
+bool debounceButton(int pin) {
+  bool result = false;
+  unsigned long now = millis();
+  int reading = digitalRead(pin);
+  if (reading != buttonState[pin])
+    lastButtonTime[pin] = now;
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if (abs(now - lastButtonTime[pin]) > DEBOUNCE) {
+    if (reading != buttonState[pin])
+      buttonState[pin] = reading;
+    if (buttonState[pin] == LOW) {
+      if (now - lastButtonState[pin] > REPEATDELAY) {
+        Serial.print("Button: ");
+        Serial.println(pin);
+        result = true;
+        lastButtonState[pin] = now;
+      }
+    }
   }
 
-
-  Serial.print("IP number assigned by DHCP is ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Starting UDP");
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
+  return result;
 }
 
+TimeChangeRule *tcr;        //pointer to the time change rule, use to get TZ abbrev
+void MorningAlarm() {
+  Serial.println("Good morning!");
+  time_t utc = now();
+  time_t local = myTZ.toLocal(utc, &tcr);
+  printTime(local, tcr -> abbrev);
+  sunrise.StartSunrise();
+}
 
-unsigned long lastButton[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-bool debounceButton(int pin) {
-  unsigned long now = millis();
-  if (digitalRead(pin) == LOW && abs(now - lastButton[pin]) > DEBOUNCE) {
-    lastButton[pin] = now;
-    Serial.print("Button: ");
-    Serial.println(pin);
-    return true;
-  } else {
-    return false;
-  }
+void EveningAlarm() {
+  Serial.println("Good evening!");
+  time_t utc = now();
+  time_t local = myTZ.toLocal(utc, &tcr);
+  printTime(local, tcr -> abbrev);
+  sunrise.StartSunset();
+}
+
+void MoonAlarm() {
+  Serial.println("Watch out for wherewolves!");
+  time_t utc = now();
+  time_t local = myTZ.toLocal(utc, &tcr);
+  printTime(local, tcr -> abbrev);
+  sunrise.StartMoonrise();
+}
+
+void MoonSetAlarm() {
+  Serial.println("Whew!");
+  time_t utc = now();
+  time_t local = myTZ.toLocal(utc, &tcr);
+  printTime(local, tcr -> abbrev);
+
+  sunrise.StartMoonset();
 }
 
 long lastTime = 0;
-TimeChangeRule *tcr;        //pointer to the time change rule, use to get TZ abbrev
 time_t utc, local;
 
-void loop() {
-  sunrise.Update();
-  static bool bSunrise = true;
-  static bool bSunset = false;
+bool alarmsSet = false;
+int morningIndex = -1;
+int eveningIndex = -1;
 
-  if (debounceButton(0)) {
-    if (!bSunrise && !bSunset) {
-      bSunrise = true;
-      sunrise.StartSunrise();
-    } else if (bSunrise && !bSunset) {
-      bSunrise = false;
-      bSunset = true;
-      sunrise.StartSunset();
-    } else if (!bSunrise && bSunset) {
-      bSunrise = true;
-      bSunset = false;
-    }
-  }
-  long milliseconds = millis();
-  if (milliseconds >= lastTime + 1000) {
-    local = myTZ.toLocal(utc, &tcr);
-    printTime(local, tcr -> abbrev);
-  }
-
+int createAlarmUTC(int h, int m, OnTick_t onTickHandler) {
+  TimeElements t;
+  t.Second = 0;
+  t.Minute = m;
+  t.Hour = h;
+  t.Day = 18;
+  t.Month = 11;
+  t.Year = 2017 - 1970;
+  time_t utc = makeTime(t);
+  return Alarm.alarmRepeat(hour(utc), m, 0, onTickHandler);
 }
 
-void loop3() {
+int createAlarm(int h, int m, OnTick_t onTickHandler) {
+  TimeElements t;
+  t.Second = 0;
+  t.Minute = m;
+  t.Hour = h;
+  t.Day = 18;
+  t.Month = 11;
+  t.Year = 2017 - 1970;
+  time_t localTime = makeTime(t);
+  time_t utc = myTZ.toUTC(localTime);
+  return Alarm.alarmRepeat(hour(utc), m, 0, onTickHandler);
+}
+
+void setupAlarms() {
+  if (!alarmsSet) {
+    for (int i = 0; i < 100; i++) {
+      Alarm.free(i);
+    }
+
+    getSunriseSunsetTimes();
+    createAlarm(22, 0, MoonAlarm);
+    createAlarm(23, 0, MoonSetAlarm);
+    alarmsSet = true;
+  }
+}
+
+/*void loop3() {
   static long last = 0;
   static int i = 0;
   if (abs(millis() - last) > 200) {
@@ -111,7 +158,7 @@ void loop3() {
     if (i > 205)
       i = 0;
   }
-}
+  }*/
 
 /*-------- NTP code ----------*/
 
@@ -122,6 +169,7 @@ time_t getNtpTime()
 {
   while (Udp.parsePacket() > 0) ; // discard any previously received packets
   Serial.println("Transmit NTP Request");
+  WiFi.hostByName(ntpServerName, timeServer);
   sendNTPpacket(timeServer);
   uint32_t beginWait = millis();
   while (millis() - beginWait < 1500) {
@@ -167,36 +215,176 @@ void sendNTPpacket(IPAddress &address)
 
 void printTime(time_t t, char *tz)
 {
-    sPrintI00(hour(t));
-    sPrintDigits(minute(t));
-    sPrintDigits(second(t));
-    Serial.print(' ');
-    Serial.print(dayShortStr(weekday(t)));
-    Serial.print(' ');
-    sPrintI00(day(t));
-    Serial.print(' ');
-    Serial.print(monthShortStr(month(t)));
-    Serial.print(' ');
-    Serial.print(year(t));
-    Serial.print(' ');
-    Serial.print(tz);
-    Serial.println();
+  sPrintI00(hour(t));
+  sPrintDigits(minute(t));
+  sPrintDigits(second(t));
+  Serial.print(' ');
+  Serial.print(dayShortStr(weekday(t)));
+  Serial.print(' ');
+  sPrintI00(day(t));
+  Serial.print(' ');
+  Serial.print(monthShortStr(month(t)));
+  Serial.print(' ');
+  Serial.print(year(t));
+  Serial.print(' ');
+  Serial.print(tz);
+  Serial.println();
 }
 
 //Print an integer in "00" format (with leading zero).
 //Input value assumed to be between 0 and 99.
 void sPrintI00(int val)
 {
-    if (val < 10) Serial.print('0');
-    Serial.print(val, DEC);
-    return;
+  if (val < 10) Serial.print('0');
+  Serial.print(val, DEC);
+  return;
 }
 
 //Print an integer in ":00" format (with leading zero).
 //Input value assumed to be between 0 and 99.
 void sPrintDigits(int val)
 {
-    Serial.print(':');
-    if(val < 10) Serial.print('0');
-    Serial.print(val, DEC);
+  Serial.print(':');
+  if (val < 10) Serial.print('0');
+  Serial.print(val, DEC);
 }
+
+void getSunriseSunsetTimes() {
+  WiFiClient client;
+  const int httpPort = 80;
+  const char* host = "api.sunrise-sunset.org";
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/json?lat=38.581572&lng=-121.494400&formatted=0";
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    line = line.substring(1);
+    if (line[0] == '{') {
+      StaticJsonBuffer<1000> jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(line.substring(line.indexOf(':') + 1, line.lastIndexOf(',')));
+      String sunrise = root.get<String>("sunrise");
+      String sunset = root.get<String>("sunset");
+
+      Serial.print("Sunrise: ");
+
+      String sunriseHour = sunrise.substring(sunrise.indexOf('T') + 1, sunrise.indexOf(':'));
+      String sunriseMin = sunrise.substring(sunrise.indexOf(':') + 1, sunrise.indexOf(':') + 3);
+      String sunsetHour = sunset.substring(sunset.indexOf('T') + 1, sunset.indexOf(':'));
+      String sunsetMin = sunset.substring(sunset.indexOf(':') + 1, sunset.indexOf(':') + 3);
+      if (morningIndex > -1)
+        Alarm.free(morningIndex);
+      if (eveningIndex > -1)
+        Alarm.free(eveningIndex);
+      morningIndex = createAlarmUTC(sunriseHour.toInt(), sunriseMin.toInt(), MorningAlarm);
+      eveningIndex = createAlarmUTC(sunsetHour.toInt(), sunsetMin.toInt(), EveningAlarm);
+      break;
+    }
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+}
+
+void setup() {
+  //Serial.begin(74880);
+  Serial.begin(115200);
+  delay(100);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("NEO_PIN: ");
+  Serial.println(NEO_PIN);
+  Serial.print("BUTTON: ");
+  Serial.println(BUTTON_PIN);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+
+
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+}
+
+void loop() {
+  Alarm.delay(0);
+  sunrise.Update();
+  static bool bSunrise = false;
+  static bool bSunset = false;
+  static bool bMoonrise = false;
+
+  if (debounceButton(BUTTON_PIN)) {
+    if (!bSunrise && !bSunset && !bMoonrise) {
+      bSunrise = true;
+      Serial.println("Sunrise");
+      sunrise.StartSunrise();
+    } else if (bSunrise && !bSunset && !bMoonrise) {
+      Serial.println("Sunset");
+      bSunrise = false;
+      bSunset = true;
+      sunrise.StartSunset();
+    } else if (!bSunrise && bSunset && !bMoonrise) {
+      Serial.println("Moonrise");
+      bSunrise = false;
+      bSunset = false;
+      bMoonrise = true;
+      sunrise.StartMoonrise();
+    } else {
+      Serial.println("Moonset");
+      bSunrise = false;
+      bSunset = false;
+      bMoonrise = false;
+      sunrise.StartMoonset();
+    }
+  }
+
+  long milliseconds = millis();
+  if (milliseconds >= lastTime + 30000) {
+    utc = now();
+    local = myTZ.toLocal(utc, &tcr);
+    printTime(local, tcr -> abbrev);
+    lastTime = milliseconds;
+    setupAlarms();
+  }
+
+}
+
+
